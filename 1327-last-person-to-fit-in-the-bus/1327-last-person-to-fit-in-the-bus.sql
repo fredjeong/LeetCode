9825ef66f1e8567ab1a04b176be0b59d3cc8853b
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-09-03 01:09:30 +0100

    Time: 872 ms (56.58%), Space: 0B (100%) - LeetHub

---
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,10 @@
+with temp as (
+    select person_name, weight, turn, sum(weight) over (order by turn) as total 
+    from queue
+)
+
+select person_name
+from temp
+where total <= 1000
+order by total desc
+limit 1
