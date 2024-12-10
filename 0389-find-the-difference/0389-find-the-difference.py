95ba1c211ba56b3769199b6d21bbaadd5804180f
Fred Jeong <100669646+fredjeong@users.noreply.github.com>
2024-12-10 20:38:07 +0900

    Time: 3 ms (55.19%), Space: 17.5 MB (6.26%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,14 +2,11 @@ from collections import defaultdict
 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        dic_s = defaultdict(int)
-        dic_t = defaultdict(int)
+        s = sorted(s)
+        t = sorted(t)
 
-        for char in s:
-            dic_s[char] += 1
-        for char in t:
-            dic_t[char] += 1
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return t[i]
         
-        for char in dic_t.keys():
-            if dic_s[char] != dic_t[char]:
-                return char
+        return t[-1]
